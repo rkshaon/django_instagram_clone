77798331f47597f6c3061be4615bb11e4395156f
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-02-20 18:05:12 +0600

    register

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from post.models import *
 
-# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Follow)
+admin.site.register(Stream)
