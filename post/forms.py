d91ed81b648a3203e3a4dd362414d6e4675a959a
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-02-24 01:18:27 +0600

    working on form

--- post/forms.py
+++ post/forms.py
@@ -5,3 +5,8 @@ class NewPostForm(forms.ModelForm):
     """docstring for NewPostForm."""
     picture = forms.ImageField(required=True)
     caption = forms.CharField(widget=forms.Textarea(attrs={'class': 'input is-mediam'}), required=True)
+    tags = forms.CharField(widget=forms.TextInput(attrs={'class': 'input is-medium'}), required=True)
+
+    class Meta:
+        model = Post
+        fields = ('picture', 'caption', 'tags')
