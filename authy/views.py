de66ed82de4047d6c61dbfd4a374dc78c088c0a9
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-02-26 02:11:35 +0600

    profile done

--- authy/views.py
+++ authy/views.py
@@ -1,15 +1,46 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse
 from django.core.paginator import Paginator
 from django.urls import resolve
+from django.contrib.auth.models import User
 
 from authy.forms import SignupForm
-from django.contrib.auth.models import User
+from authy.models import Profile
+from post.models import Post, Follow
+
 
 def UserProfile(request, username):
     user = get_object_or_404(User, username=username)
-    pass
+    profile = Profile.objects.get(user=user)
+    url_name = resolve(request.path).url_name
+
+    if url_name == 'profile':
+        posts = Post.objects.filter(user=user).order_by('-posted')
+    else:
+        posts = profile.favorites.all()
+
+    # profile info stats
+    post_count = Post.objects.filter(user=user).count()
+    following_count = Follow.objects.filter(follower=user).count()
+    follower_count = Follow.objects.filter(following=user).count()
+
+    # paginator
+    paginator = Paginator(posts, 6)
+    page_number = request.GET.get('page')
+    posts_paginator = paginator.get_page(page_number)
+
+    context = {
+        'posts': posts_paginator,
+        'profile': profile,
+        'url_name': url_name,
+        'post_count': post_count,
+        'following_count': following_count,
+        'follower_count': follower_count,
+    }
+    template = loader.get_template('profile.html')
+    return HttpResponse(template.render(context, request))
+
 
 def Signup(request):
     if request.method == 'POST':
--- instagram_clone/urls.py
+++ instagram_clone/urls.py
@@ -18,8 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from authy.views import UserProfile
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('authy.urls')),
     path('post/', include('post.urls')),
+    path('<username>/', UserProfile, name='profile'),
+    path('<username>/saved', UserProfile, name='profile_favorite'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
