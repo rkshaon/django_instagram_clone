65a96ab2846111715551df90cb0e80c44f6b90ab
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-03-20 18:26:54 +0600

    comment model

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from post.models import Post
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Comment(models.Model):
+    """docstring for Comment."""
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
