ad835fb35792ef48958ddba23c539f8000e4947b
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-03-21 00:30:41 +0600

    dm done

--- direct_message/urls.py
+++ direct_message/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from direct_message.views import inbox
+from direct_message.views import inbox, direct_message, send_dm
 
 urlpatterns = [
     path('', inbox, name='inbox'),
+    path('message/<username>', direct_message, name='direct_message'),
+    path('send/', send_dm, name='send_dm'),
 ]
--- direct_message/views.py
+++ direct_message/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.template import loader
 
+from django.contrib.auth.models import User
 from direct_message.models import Message
 
 @login_required
@@ -27,7 +28,44 @@ def inbox(request):
         'messages': messages,
         'active_dm': active_dm,
     }
-    
+
     template = loader.get_template('dm/direct.html')
 
     return HttpResponse(template.render(context, request))
+
+@login_required
+def direct_message(request, username):
+    user = request.user
+    messages = Message.get_messages(user=user)
+    active_dm = username
+    directs = Message.objects.filter(user=user, recipient__username=username)
+    directs.update(is_read=True)
+
+    for message in messages:
+        if message['user'].username == username:
+            message['unread'] = 0
+
+    context = {
+        'directs': directs,
+        'messages': messages,
+        'active_dm': active_dm,
+    }
+
+    template = loader.get_template('dm/direct.html')
+
+    return HttpResponse(template.render(context, request))
+
+@login_required
+def send_dm(request):
+    from_user = request.user
+    to_user_username = request.POST.get('to_user')
+    print(request.POST)
+    body = request.POST.get('body')
+
+    if request.method == 'POST':
+        to_user = User.objects.get(username=to_user_username)
+        Message.send_message(from_user, to_user, body)
+
+        return redirect('inbox')
+    else:
+        HttpResponseBadRequest()
