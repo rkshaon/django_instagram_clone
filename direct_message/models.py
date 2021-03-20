f8429b6e335e93afca520540211432154293cfb8
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-03-20 22:57:16 +0600

    dm model

--- direct_message/models.py
+++ direct_message/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Max
 
-# Create your models here.
+class Message(models.Model):
+    """docstring for Message."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='from_user')
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='to_user')
+    body = models.TextField(max_length=1000, blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def send_message(from_user, to_user, body):
+        sender_message = Message(
+            user = from_user,
+            sender = from_user,
+            recipient = to_user,
+            body = body,
+            is_read = True,)
+        sender_message.save()
+
+        recipient_message = Message(
+            user = to_user,
+            sender = from_user,
+            recipient = from_user,
+            body = body,
+            )
+        recipient_message.save()
+
+        return sender_message
+
+    def get_messages(user):
+        users = []
+        messages = Message.object.filter(user=user).values('recipient').annotate(last=Max('date')).order_by('-last')
+
+        for message in messages:
+            user.append({
+                'user': User.objects.get(pk=message['recipient']),
+                'last': message['last'],
+                'unread': Message.object.filter(user=user, recipient__pk=message['recipient'], is_read=False).count(),
+                })
+        return users
