7dc8eaf2b6dff3339251337069abc723a0c4598a
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-04-04 02:09:44 +0600

    dm count done

--- direct_message/views.py
+++ direct_message/views.py
@@ -72,6 +72,14 @@ def new_conversation(request, username):
 
 	return redirect('inbox')
 
+def check_dm(request):
+    directs_count = 0
+
+    if request.user.is_authenticated:
+        directs_count = Message.objects.filter(user=request.user, is_read=False).count()
+
+    return {'directs_count': directs_count}
+
 @login_required
 def send_dm(request):
     from_user = request.user
