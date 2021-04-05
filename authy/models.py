df088bcf17bcd2df9f4fe8da2cb3c133514767e6
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-04-06 00:49:04 +0600

    profile picture

--- authy/models.py
+++ authy/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from PIL import Image
+from django.conf import settings
+import os
 
 from post.models import Post
 
 def use_directory_path(instance, filename):
     # this file will be uploaded to MEDIA_ROOT/user(id)/filename
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
+    # return 'user_{0}/{1}'.format(instance.user.id, filename)
+    profile_pic_name = 'user_{0}/profile.jpg'.format(instance.user.id)
+    full_path = os.path.join(settings.MEDIA_ROOT, profile_pic_name)
+
+    if os.path.exists(full_path):
+        os.remove(full_path)
+
+    return profile_pic_name
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -18,6 +28,16 @@ class Profile(models.Model):
     created = models.DateField(auto_now_add=True)
     favorites = models.ManyToManyField(Post)
     picture = models.ImageField(upload_to=use_directory_path, blank=True, null=True, verbose_name='Picture')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        SIZE = 300, 300
+
+        if self.picture:
+            pic = Image.open(self.picture)
+            pic.thumbnail(SIZE, Image.LANCZOS)
+            pic.save(self.picture.path)
 
     def __str__(self):
         return str(self.user)
