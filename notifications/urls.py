dc85dfb4449209a76790c57ab8d5f3f555c0ea06
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-04-05 23:07:00 +0600

    notification delete

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from notifications.views import show_notification
+from notifications.views import show_notification, delete_notification
 
 urlpatterns = [
     path('', show_notification, name='show_notification'),
+    path('<noti_id>/delete', delete_notification, name='delete_notification'),
 ]
--- notifications/views.py
+++ notifications/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
 
@@ -15,3 +15,9 @@ def show_notification(request):
     }
 
     return HttpResponse(template.render(context, request))
+
+def delete_notification(request, noti_id):
+    user = request.user
+    Notification.objects.filter(id=noti_id, user=user).delete()
+
+    return redirect('show_notification')
